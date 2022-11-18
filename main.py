0bd3f223c772ea6efc9555b1f5cc95a781b7ea64
Hart1lt <timsad80>
2022-11-18 13:04:26 +0300

    solution

--- main.py
+++ main.py
@@ -1,1 +1,37 @@
-print('hi')
+import sys
+
+from PyQt5 import uic
+from PyQt5.QtGui import QPainter, QPen, QColor
+from PyQt5.QtWidgets import QWidget, QApplication
+from random import randint
+
+
+class Exemple(QWidget):
+    def __init__(self):
+        super().__init__()
+        uic.loadUi('UI.ui', self)
+        self.do_paint = False
+        self.pushButton.clicked.connect(self.paint)
+
+    def paintEvent(self, event):
+        if self.do_paint:
+            qp = QPainter()
+            qp.begin(self)
+            self.draw_flag(qp)
+            qp.end()
+
+    def paint(self):
+        self.do_paint = True
+        self.repaint()
+
+    def draw_flag(self, qp):
+        qp.setBrush(QColor(255, 255, 0))
+        x = randint(50, 250)
+        qp.drawEllipse(30, 30, x, x)
+
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    w = Exemple()
+    w.show()
+    sys.exit(app.exec_())
